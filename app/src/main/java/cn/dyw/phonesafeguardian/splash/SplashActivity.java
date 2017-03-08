0cde54f11248785ddc8dbb7c020b009a6f7b371d
dyw <dyw770@qq.com>
2017-03-08 18:04:20 +0800

    初始化项目

---
+++ app/src/main/java/cn/dyw/phonesafeguardian/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package cn.dyw.phonesafeguardian.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.dyw.phonesafeguardian.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}

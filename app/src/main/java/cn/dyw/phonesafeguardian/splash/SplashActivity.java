0656f427211778bf86244d18b2c9514788888d56
dyw <dyw770@qq.com>
2017-03-09 21:57:39 +0800

    显示本地版本号

--- app/src/main/java/cn/dyw/phonesafeguardian/splash/SplashActivity.java
+++ app/src/main/java/cn/dyw/phonesafeguardian/splash/SplashActivity.java
@@ -1,16 +1,36 @@
 package cn.dyw.phonesafeguardian.splash;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import cn.dyw.phonesafeguardian.R;
 
 public class SplashActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
+
+        textView = (TextView) findViewById(R.id.version);
+
+        /**
+         * 获取当前本地版本号，并在欢迎界面进行显示
+         */
+        PackageManager manger = SplashActivity.this.getPackageManager();
+        try {
+            PackageInfo info = manger.getPackageInfo(SplashActivity.this.getPackageName(), 0);
+            Log.d("testttt", info.versionName);
+            textView.setText("版本号：" + info.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            Log.d("testttt", "45555");
+        }
     }
 }

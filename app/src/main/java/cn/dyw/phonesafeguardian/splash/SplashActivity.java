a80763feaadcde1dfc056d4c623933233261e2da
dyw <dyw770@qq.com>
2017-03-09 21:02:24 +0800

    readme.me新增了项目命名规则，编写了欢迎界面xml

--- app/src/main/java/cn/dyw/phonesafeguardian/splash/SplashActivity.java
+++ app/src/main/java/cn/dyw/phonesafeguardian/splash/SplashActivity.java
@@ -10,6 +10,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_splash);
     }
 }
